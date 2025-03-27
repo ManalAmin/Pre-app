ef04a4c9b9d33e7d2333b3a17b5e6ef0c19cc53e
Manal Amin <manalamin657@gmail.com>
2025-03-27 16:06:32 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './index.css';
+function App() {
+
+  return (
+    <>
+    <h1 className='mb-4'>
+      HELLO WORLD !
+    </h1>
+        <p>Manal Amin</p>
+    </>
+  )
+}
+
+export default App
